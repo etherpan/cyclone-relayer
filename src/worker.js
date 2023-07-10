06bec4e09777501692a4d64ea0960e1558e69937
jovoel <kryptoexpert1@gmail.com>
2023-07-11 00:22:23 +0200

    reote||

--- src/worker.js
+++ src/worker.js
@@ -163,7 +163,7 @@ async function checkTornadoFee({ args, contract }) {
 
 async function getProxyContract() {
   let proxyAddress
-  if (netId === 5 || ) {
+  if (netId === 5) {
     proxyAddress = tornadoGoerliProxy
   } else {
     // proxyAddress = await resolver.resolve(torn.tornadoRouter.address)
